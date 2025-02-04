dc34869821c1c32415c115b6b9d4d1b0b4ea01fb
Airo0429 <91652791+Airo0429@users.noreply.github.com>
2025-02-04 19:53:43 +0800

    create the supabase client

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://qezrykhmmizwqarjregu.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlenJ5a2htbWl6d3FhcmpyZWd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzMzY5NzIsImV4cCI6MjA1MzkxMjk3Mn0.7zxXcFfDUVpEdah4W1sNU9O0hR7ten-C00gBk7H5ek4')
