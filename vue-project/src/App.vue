35d904f395900174f2256a88e5e4a9c5a599a136
Airo0429 <91652791+Airo0429@users.noreply.github.com>
2025-02-18 08:17:10 +0800

    removed <country/>
      <food-item/> 
      <food-item2/>
      <food-item/> 
      <food-item2/> 
      <comment-form/>
      <comment/>

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,7 @@
 <template>
   <h1>Food</h1>
   <rest-api/>
-  <country/>
- 
-  <food-item/> 
-  <food-item2/>
-  <food-item/> 
-  <food-item2/> 
-  <comment-form/>
-  <comment/>
- 
+  
 </template>
 
 <style></style>
